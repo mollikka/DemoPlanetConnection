9278062c588348c65dd34db6e5ed522205713c9e
Lauri Tervonen <lauri.tervonen@reaktor.com>
2023-10-29 00:21:47 +0300

    Sync to audio time
    allows pause

--- src/main.ts
+++ src/main.ts
@@ -6,19 +6,18 @@ import fragmentShaderSrc from "./glsl/lesson03.frag";
 import { vec3 } from "./vectors";
 
 export const run = async () => {
-  await delay(1000);
-
   const canvas = document.querySelector<HTMLCanvasElement>("canvas")!;
   canvas.style.width = "100%";
   canvas.width = config.canvas.width;
   canvas.height = config.canvas.height;
 
-  const audio = new Promise<HTMLAudioElement>((resolve, reject) => {
+  const audio = await new Promise<HTMLAudioElement>((resolve, reject) => {
     const audio = new Audio("music.ogg");
     audio.volume = 0.1;
     audio.oncanplay = () => resolve(audio);
     audio.onerror = reject;
     resolve(audio);
+    audio.currentTime = 0;
   });
 
   const gl = canvas.getContext("webgl2")!;
@@ -28,14 +27,10 @@ export const run = async () => {
   const shader = new ShaderProgram(gl, vertexShaderSrc, fragmentShaderSrc);
   const [shaderVertexPos] = shader.vertexAttributes("VERTEX_POS");
 
-  const startTime = new Date().getTime();
   const bpm = 120;
 
-  await audio;
-
   const renderNext = (time: DOMHighResTimeStamp) => {
-    const now = new Date().getTime() - startTime;
-    const seconds = now * 0.001;
+    const seconds = audio.currentTime;
     const beats = (seconds / 60.0) * bpm;
 
     screen.bind(shaderVertexPos);
